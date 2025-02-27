d90a2cc1b76837dc5f498610ebc808ced93ed61a
Andy Pak <im.andy.pak@gmail.com>
2025-02-26 19:36:21 -0500

    Document segment numbers and add pictures with labels

---
+++ Documentation/maybeWeCanAddThisToReport.md
@@ -0,0 +1,16 @@
+# How we made our prototype design
+
+First we analyzed the given roundabout code and traced the movement paths of the cars while making note of the segment numbers.
+
+![Analysis](./maybeWeCanAddThisToReportImages/img1.png)
+
+Next we started to design our intersection with our main objective in mind of reduction traffic congestion.
+![Planning1](./maybeWeCanAddThisToReportImages/img2.png)
+![Planning2](./maybeWeCanAddThisToReportImages/img3.png)
+
+From there we started with expanding the number of lanes for entry and exit to allow for more cars. Then we started to work on making the roundabout have 2 lanes.
+![InitialPrototype](./maybeWeCanAddThisToReportImages/img4.png)
+
+Finally we had everything done and labeled each segment with its number so someone else can do the rest of the work (like vehicle generating)
+![Labeled1](./maybeWeCanAddThisToReportImages/img5.png)
+![Labeled2](./maybeWeCanAddThisToReportImages/img6.png)
--- build_and_run/improved_roundabout.py
+++ build_and_run/improved_roundabout.py
@@ -36,8 +36,8 @@ class Intersection:
         self.sim.create_segment((lane_space/2 + island_width/2, -intersection_size/2), (lane_space/2 + island_width/2, -length - intersection_size/2))
         self.sim.create_segment((lane_space*3/2 + island_width/2, -intersection_size/2), (lane_space*3/2 + island_width/2, -length - intersection_size/2))
 
-        self.sim.create_segment((-intersection_size/2, -lane_space/2 - island_width/2), (-length-intersection_size/2, -lane_space/2 - island_width/2))
-        self.sim.create_segment((-intersection_size/2, -lane_space*3/2 - island_width/2), (-length-intersection_size/2, -lane_space*3/2 - island_width/2))
+        self.sim.create_segment((-intersection_size/2, -lane_space/2 - island_width/2), (-length - intersection_size/2, -lane_space/2 - island_width/2))
+        self.sim.create_segment((-intersection_size/2, -lane_space*3/2 - island_width/2), (-length - intersection_size/2, -lane_space*3/2 - island_width/2))
 
         #corners 16-19
         self.sim.create_quadratic_bezier_curve((lane_space + island_width/2, radius),(radius,radius),(radius,lane_space + island_width/2))
@@ -52,7 +52,6 @@ class Intersection:
         self.sim.create_segment((-lane_space - island_width/2, radius),(lane_space + island_width/2, radius))
 
         #turn into corners 24-31
-        # end of entrance then entrance to corner then pull point x1, y2
         self.sim.create_quadratic_bezier_curve((lane_space/2 + island_width/2, intersection_size/2),(lane_space/2 + island_width/2, radius),(lane_space + island_width/2, radius))
         self.sim.create_quadratic_bezier_curve((lane_space*3/2 + island_width/2, intersection_size/2),(lane_space*3/2 + island_width/2, radius),(lane_space*3/2 + island_width/2, radius))
 
@@ -91,7 +90,6 @@ class Intersection:
         self.sim.create_segment((-lane_space - island_width/2, radius - lane_space),(lane_space + island_width/2, radius - lane_space))
 
         #turn into inside corners 48-55
-        # end of entrance then entrance to corner then pull point x1, y2
         self.sim.create_quadratic_bezier_curve((lane_space/2 + island_width/2, intersection_size/2),(lane_space/2 + island_width/2, radius - lane_space),(lane_space + island_width/2, radius - lane_space))
         self.sim.create_quadratic_bezier_curve((lane_space*3/2 + island_width/2, intersection_size/2),(lane_space*3/2 + island_width/2, radius - lane_space),(lane_space*3/2 + island_width/2, radius - lane_space))
 
@@ -120,38 +118,40 @@ class Intersection:
         # TODO: FIX THE PATHS OF ALL THE VHEILCES AND ADD NEW ONES FOR THE NEW INNER ROUNDABOUT + ADD COLORS
         self.vg = VehicleGenerator({
             'vehicles': [
-                # (1, {'path': [0, 24, 16,32,8],'v_max':self.v}),
-                # (1, {'path': [0, 24, 16,20,17,33,9],'v_max':self.v}),
-                # (1, {'path': [0, 24, 16,20,17,21,18,34,10],'v_max':self.v}),
-                # (1, {'path': [0, 24, 16,20,17,21,18,22,19,35,11],'v_max':self.v}),
+                # these dont propperly work TODO someone fix
+                (1, {'path': [0, 24, 16,32,8],'v_max':self.v}),
+                (1, {'path': [0, 24, 16,20,17,33,9],'v_max':self.v}),
+                (1, {'path': [0, 24, 16,20,17,21,18,34,10],'v_max':self.v}),
+                (1, {'path': [0, 24, 16,20,17,21,18,22,19,35,11],'v_max':self.v}),
 
-                # (1, {'path': [1, 25, 17,33,9],'v_max':self.v}),
-                # (1, {'path': [1, 25, 17,21,18,34,10],'v_max':self.v}),
-                # (1, {'path': [1, 25, 17,21,18,22,19,35,11],'v_max':self.v}),
-                # (1, {'path': [1, 25, 17,21,18,22,19,15,16,32,8],'v_max':self.v}),
+                (1, {'path': [1, 25, 17,33,9],'v_max':self.v}),
+                (1, {'path': [1, 25, 17,21,18,34,10],'v_max':self.v}),
+                (1, {'path': [1, 25, 17,21,18,22,19,35,11],'v_max':self.v}),
+                (1, {'path': [1, 25, 17,21,18,22,19,15,16,32,8],'v_max':self.v}),
 
-                # (1, {'path': [2, 26, 18,34,10],'v_max':self.v}),
-                # (1, {'path': [2, 26, 18,22,19,35,11],'v_max':self.v}),
-                # (1, {'path': [2, 26, 18,22,19,15,16,32,8],'v_max':self.v}),
-                # (1, {'path': [2, 26, 18,22,19,15,16,20,17,33,9],'v_max':self.v}),
+                (1, {'path': [2, 26, 18,34,10],'v_max':self.v}),
+                (1, {'path': [2, 26, 18,22,19,35,11],'v_max':self.v}),
+                (1, {'path': [2, 26, 18,22,19,15,16,32,8],'v_max':self.v}),
+                (1, {'path': [2, 26, 18,22,19,15,16,20,17,33,9],'v_max':self.v}),
                 
-                # (1, {'path': [3, 27, 19,35,11],'v_max':self.v}),
-                # (1, {'path': [3, 27, 19,15,16,32,8],'v_max':self.v}),
-                # (1, {'path': [3, 27, 19,15,16,20,17,33,9],'v_max':self.v}),
-                # (1, {'path': [3, 27, 19,15,16,20,17,21,18,34,10],'v_max':self.v}),
+                (1, {'path': [3, 27, 19,35,11],'v_max':self.v}),
+                (1, {'path': [3, 27, 19,15,16,32,8],'v_max':self.v}),
+                (1, {'path': [3, 27, 19,15,16,20,17,33,9],'v_max':self.v}),
+                (1, {'path': [3, 27, 19,15,16,20,17,21,18,34,10],'v_max':self.v}),
 
                 # Testing Paths
                 # delete for final this just to test the inner roundabout
-                # (1, {'path': [48, 40, 44, 41, 45, 42, 46, 43, 8],'v_max':self.v}),
+                # (1, {'path': [44, 45, 46, 47],'v_max':self.v}),
                 # (1, {'path': [48, 49, 50, 51, 52, 53, 54, 55],'v_max':self.v}), # test all turn into corners
-                (1, {'path': [56, 57, 58, 59, 60, 61, 62, 63], 'v_max':self.v}), # test all turn to exit from inside
+                # (1, {'path': [56, 57, 58, 59, 60, 61, 62, 63], 'v_max':self.v}), # test all turn to exit from inside
             ], 'vehicle_rate': 20
         })
         
-        self.sim.define_interfearing_paths([0,24],[15,16],turn=True)
-        self.sim.define_interfearing_paths([1,25],[20,17],turn=True)
-        self.sim.define_interfearing_paths([2,26],[21,18],turn=True)
-        self.sim.define_interfearing_paths([3,27],[22,19],turn=True)
+        # TODO: WE NEED TO FIX THIS AND DETERMINE THE INTERFERING PATHS (lowkey we dont need to do this)
+        # self.sim.define_interfearing_paths([0,24],[15,16],turn=True)
+        # self.sim.define_interfearing_paths([1,25],[20,17],turn=True)
+        # self.sim.define_interfearing_paths([2,26],[21,18],turn=True)
+        # self.sim.define_interfearing_paths([3,27],[22,19],turn=True)
         self.sim.add_vehicle_generator(self.vg)
     
     def get_sim(self):
